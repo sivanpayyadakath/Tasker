72c9c92d3c12a1893cea4d9b780acd562f37263f
sivanpayyadakath <sivan.payyadakath@gmail.com>
2018-11-26 18:13:56 +0900

    git-slack integration

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,16 @@
 class TodosController < ApplicationController
 
   def create
+
   end
 
   def index
+    @todos = Todo.all
+  end
+
+
+private
+  def todo_params
+    params.require(:todo).permit(:content)
   end
 end
