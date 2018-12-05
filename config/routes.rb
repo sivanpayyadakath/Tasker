09b17d2bac086f3beccb3f5d907ae833059fe36f
sivanpayyadakath <sivan.payyadakath@gmail.com>
2018-12-05 16:13:30 +0900

    signup of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'users#new'
+
+  post '/signup', to: 'users#create'
+
   resources :users do
     resources :todos do
       member do
@@ -9,6 +12,6 @@ Rails.application.routes.draw do
         patch :task_started
       end
     end
-    root 'todos#index'
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-# User.create!(
-#      name: 'Random guy',
-#      email: 'randomguy@gmail.com'
+ User.create!(
+      name: 'the picture guy',
+      email: 'pictureguy@gmail.com'
+ )
+
+#
+# 3.times do
+#   Todo.create!(
+#       title: Faker::Commerce.product_name,
+#       content: Faker::Lorem.sentence(5),
+#       deadline_at: Faker::Date.forward,
+#       priority: 3,
+#       status: "not started",
+#       user_id: 2
+#   )
+# end
+#
+# Todo.create!(
+#     title: Faker::Commerce.product_name,
+#     content: Faker::Lorem.sentence(5),
+#     deadline_at: Time.now + 2.minutes,
+#     priority: 2,
+#     status: "not started",
+#     user_id: 2
 # )
-
-
-3.times do
-  Todo.create!(
-      title: Faker::Commerce.product_name,
-      content: Faker::Lorem.sentence(5),
-      deadline_at: Faker::Date.forward,
-      priority: 3,
-      status: "not started",
-      user_id: 2
-  )
-end
-
-Todo.create!(
-    title: Faker::Commerce.product_name,
-    content: Faker::Lorem.sentence(5),
-    deadline_at: Time.now + 2.minutes,
-    priority: 2,
-    status: "not started",
-    user_id: 2
-)
